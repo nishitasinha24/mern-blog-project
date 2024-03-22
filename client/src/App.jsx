c1a4222a74dbe2a4778505982ddd7b036c960ff4
nishitasinha24 <nishita.sinha24@gmail.com>
2024-03-22 14:14:51 -0400

    Add Footer Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Footer from './components/Footer'
 import Header from './components/Header'
 import About from './pages/About'
 import Dashboard from './pages/Dashboard'
@@ -19,8 +20,8 @@ export default function App() {
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/projects" element={<Projects />} />
-
       </Routes>
+      <Footer />
     </BrowserRouter>
   )
 }
